633d03b950bbf73ebbb9939c8a28175cd2959a3f
Claudius <opensource@amenthes.de>
2018-05-21 02:58:37 +0200

    example MQTT Client rudimentarily added

---
+++ Assets/Scripts/MQTTClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class MQTTClient : MonoBehaviour {
+	public string hostname;
+	public int port;
+	public string topic;
+
+	public Vector3 direction;
+	private MqttClient client;
+
+	// Use this for initialization
+	void Start () {
+		client = new MqttClient(hostname, port, false, null, null, MqttSslProtocols.None);
+		client.MqttMsgPublishReceived += client_MqttReceived;
+
+		client.Connect("unity");
+		client.Subscribe(new string[] {topic}, new byte[] {MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE});
+	}
+
+	void client_MqttReceived(object sender, MqttMsgPublishEventArgs e) {
+		string msg = Encoding.UTF8.GetString(e.Message);
+		Debug.Log(string.Format("Received ({0}): {1}", e.Topic, msg));
+		string[] bits = msg.Split(',');
+		direction.x = float.Parse(bits[0]);
+		direction.y = float.Parse(bits[1]);
+		direction.z = float.Parse(bits[2]);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public Vector3 rotation;
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(rotation);
+        transform.LookAt(player.transform);
     }
 }
